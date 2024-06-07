52ac9a1ea23def40ef6e7f132d7f21ce301a9d3c
JoeyCorbett <corbettjoey9@gmail.com>
2024-06-07 10:30:32 -0400

    Add calculator functionality. (display multiple digit nums, clear display)
    Numbers can properly be displayed now without replacing themselves and operator values will be searched for through an object to return the right operator for future use.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let firstNum;
 let operator;
 let secondNum;
-let displayVar;
+let displayValueOne;
+let displayValueTwo;
 
-const numBtns = document.querySelectorAll(".nums, .zero");
-let displayNum = document.querySelector(".display-numbers")
+const numBtns = document.querySelectorAll(".input");
+let displayNum = document.querySelector(".display-numbers");
+let clearBtn = document.querySelector(".clear");
+let operators = document.querySelectorAll(".operator")
+
+let operatorValues = {
+    divide: "/",
+    multiply: "*",
+    add: "+",
+    subtract: "-"
+}
 
 
 function sum(a, b) {
@@ -24,19 +34,54 @@ function divide(a, b) {
 }
 
 function operate(op, a, b) {
-    op(a, b);
+    return op(a, b);
 }
 
 function populateDisplay(num) {
-    displayNum.textContent = num;
-    // displayVar = displayNum.textContent;
+    if (displayNum.textContent === "0") {
+        displayNum.textContent = num;
+        displayValueOne = num;
+    } else {
+        displayNum.textContent += num;
+        displayValueOne = displayNum.textContent;
+    }
+}
+
+function manageNums(operator) {
+    console.log(operator);
 }
 
 numBtns.forEach(btn => {
     btn.addEventListener("click", (e) => {
         let value = e.target.innerHTML
         populateDisplay(value);
-        displayVar = value;        
     });
 });
+
+
+
+clearBtn.addEventListener("click", () => {
+    displayNum.textContent = "0";
+});
+
+operators.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let value = e.target.innerHTML;
+        for (let key in operatorValues) {
+            if (value == operatorValues[key]) {
+                value = key;
+            }
+        }
+        manageNums(value);
+    });
+});
+
+
+
+
+// Store first and second number input
+    // Figure out how to store 2+ digit numbers
+// Utilize operator that user selects
+// Operate on two numbers when user presses "="
+// Once operate() called udpate display with solution of operation
  
